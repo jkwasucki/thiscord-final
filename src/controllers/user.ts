cf5c194d3d3e505d1f1c92932ef6abfbc0988836
azherid <kwakwasucki@wp.pl>
2023-10-30 19:28:40 +0100

    Final

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -75,7 +75,8 @@ export const LoginUser:RequestHandler = async(req,res,next) =>{
         const expirationTime = 12 * 60 * 60 * 1000; // 12 hours in milliseconds
         res.cookie("token", token, {
             expires: new Date(Date.now() + expirationTime),
-            domain: 'thiscord-ten.vercel.app',
+            domain: '.ondigitalocean.app',
+            
             httpOnly: false,
             secure: true
         })
