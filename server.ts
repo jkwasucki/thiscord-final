cb0df991235eb691f211cd32c753ff5ed5d1167f
azherid <kwakwasucki@wp.pl>
2023-10-30 17:00:28 +0100

    Final

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ const CLIENT_BASE_URL:string = process.env.NODE_ENV === 'production' ? 'https://
 
 const app = express()
 const corsOptions = {
-    origin: true,
+    origin: CLIENT_BASE_URL,
     credentials: true,
 };
 
